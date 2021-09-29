b0305cb96d89e8b6e6109ff71f93a560ccaa3923
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-09-29 09:32:37 +0100

    changes

--- index.js
+++ index.js
@@ -1,49 +1,19 @@
-function findSequence(input) {
-    const predecessors = [];
-    const subsequenceIndices = [];
-    let subsequenceLength = 0;
-  
-    for (let i = 0; i < input.length; i++) {
-        // Binary search for the largest positive `j ≤ L`
-        //  such that `input[subsequenceIndices[j]] < input[i]`
-        let low = 1, high = subsequenceLength;
-        while (low <= high) {
-            let middle = Math.ceil((low + high)/2);
-            if (input[subsequenceIndices[middle]] < input[i])
-                low = middle + 1;
-            else
-                high = middle - 1;
-        }
+function findSequence (obj) {
+    let jamil = []
+    let numStages = obj.length;
 
-        // After searching, `low` is 1 greater than the
-        //  length of the longest prefix of `input[i]`
-        let newLength = low;
+    ajoke = true;
 
-        // The predecessor of `input[i]` is the last index of 
-        //  the subsequence of length `newL - 1`
-        predecessors[i] = subsequenceIndices[newLength - 1];
-        subsequenceIndices[newLength] = i;
-    
-        // console.log(low, high, newLength, subsequenceLength, predecessors[i], subsequenceIndices[newLength - 1], subsequenceIndices); break;
-
-        // If we found a subsequence longer than any we've
-        //  found yet, update `subsequenceLength`
-        if (newLength > subsequenceLength)
-        subsequenceLength = newLength;
+    for (var i = 0; i < numStages; i++) {
+        console.log(obj[i]);
+        jamil.
     }
 
-    // Reconstruct the longest increasing subsequence
-    let subsequence = [];
-    let k = subsequenceIndices[subsequenceLength];
-    console.log({predecessors, subsequenceIndices, subsequenceLength, k})
-    for (let i = subsequenceLength - 1; i >= 0; i--) {
-        console.log(i, k, input[k], predecessors[k], predecessors)
-        subsequence.unshift(input[k]);
-        k = predecessors[k];
+    while (ajoke) {
+        LoopSt()
+        ajoke = false;
     }
-
-    return subsequence;
-};
+}
   
 let james = findSequence([0, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]);
-console.log(james)
+// console.log(james)
